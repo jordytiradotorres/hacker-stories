953916a45dce44930862ac4002d272edce04ab38
Jordy Tirado <jordytiradotorres20@gmail.com>
2020-12-10 00:40:04 -0500

    cambiando de fetch a axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useReducer, useState } from "react";
+import axios from "axios";
 import "./App.css";
 import List from "./components/List";
 import InputWithLabel from "./components/InputWithLabel";
@@ -25,12 +26,12 @@ const App = () => {
 			type: "STORIES_FETCH_INIT",
 		});
 
-		fetch(url)
-			.then((response) => response.json())
+		axios
+			.get(url)
 			.then((result) => {
 				dispatchStories({
 					type: "STORIES_FETCH_SUCCESS",
-					payload: result.hits,
+					payload: result.data.hits,
 				});
 			})
 			.catch(() => {
